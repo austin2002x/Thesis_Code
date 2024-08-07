bbf942a8231882f4a5d14029363b160cabe1b39d
austin2002x <a.prentice@uq.net.au>
2024-08-07 18:44:37 +1000

    COMMIT

--- pandaaahs.py
+++ pandaaahs.py
@@ -11,15 +11,21 @@ columns_to_check = ['Identification', 'Schedule Number']  # Replace with your co
 
 # Drop rows where both specified columns are NaN
 df_cleaned = df.dropna(subset=columns_to_check, how='all')
+df_cleaned_test=df_cleaned.copy()
+df_cleaned_test.drop(columns='Schedule Number',inplace=True)
+df_cleaned_test.dropna(inplace=True)
 
-#df_cleaned["Schedule Number"] = df_cleaned['Schedule Number'].astype("Int64")
+df_cleaned_test.rename(columns={'Identification': 'Schedule Number'}, inplace=True)
 
-df_cleaned['Schedule Number'] = df_cleaned['Schedule Number'].fillna(df_cleaned['Identification'])
 
-schedule_values_to_keep = [40.0,80.0,'STD','XS','XXS']
+schedule_values_to_keep = [40.0,80.0]
 filtered_df = df_cleaned[df_cleaned['Schedule Number'].isin(schedule_values_to_keep)]
 filtered_df.drop(columns='Identification',inplace=True)
 
-df_cleaned.to_csv('cleaned_data.csv', index=False)
+df_concat = pd.concat([filtered_df,df_cleaned_test],ignore_index=True)
+
+df_cleaned_test.to_csv('cleaned_data.csv', index=False)
 filtered_df.to_csv('filtered_data.csv', index=False)
+
+df_concat.to_csv("ASME_reduced.csv",index=False)
 
